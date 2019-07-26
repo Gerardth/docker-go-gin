a67f52d412a569e063efb8490873c9f302859993
Jose Garay <jgaray@asesoftware.com>
2019-07-26 15:57:33 -0500

    adding SQL server Azure database conn

--- src/main.go
+++ src/main.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
 // Product type
@@ -55,22 +57,76 @@ func main() {
 }
 
 func GetProducts(c *gin.Context) {
+	//Modificado para conectar con base de datos SQL Server
+	var db *sql.DB
+	var server = "backofficedb"
+	var port = 1433
+	var user = "backoffice"
+	var password = "D4t4c3nt3rCl0ud"
+	var database = "backofficeDB"
 
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
+	// Build connection string
+    connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+        server, user, password, port, database)
+
+    var err error
+
+    // Create connection pool
+    db, err = sql.Open("sqlserver", connString)
+    if err != nil {
+        log.Fatal("Error creating connection pool: ", err.Error())
+    }
+    ctx := context.Background()
+    err = db.PingContext(ctx)
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+	fmt.Printf("Connected!\n")
+	
+	// Read employees
+    count, err := ReadEmployees()
+    if err != nil {
+        log.Fatal("Error reading Employees: ", err.Error())
+    }
+    fmt.Printf("Read %d row(s) successfully.\n", count)
+
+	func ReadEmployees() (int, error) {
+		ctx := context.Background()
+	
+		// Check if database is alive.
+		err := db.PingContext(ctx)
+		if err != nil {
+			return -1, err
+		}
+	
+		tsql := fmt.Sprintf("SELECT Id, Name, Location FROM SalesLT.Employees;")
+	
+		// Execute query
+		rows, err := db.QueryContext(ctx, tsql)
+		if err != nil {
+			return -1, err
+		}
+	
+		defer rows.Close()
+	
+		var count int
+	
+		// Iterate through the result set.
+		for rows.Next() {
+			var name, location string
+			var id int
+	
+			// Get values from row.
+			err := rows.Scan(&id, &name, &location)
+			if err != nil {
+				return -1, err
+			}
+	
+			fmt.Printf("ID: %d, Name: %s, Location: %s\n", id, name, location)
+			count++
+		}
+	
+		return count, nil
 	}
-	defer db.Close()
-	db.AutoMigrate(&Product{})
-
-	var products []Product
-
-	if err := db.Find(&products).Error; err != nil {
-		c.AbortWithStatus(http.StatusInternalServerError)
-		log.Println(err)
-	} else {
-		c.JSON(http.StatusOK, products)
-		log.Println(products)
-	}
-
 }
+
