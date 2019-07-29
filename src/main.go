f738ea901d90f810869434e2c7e1f3df63467dd1
Jose Garay <jgaray@asesoftware.com>
2019-07-29 08:58:51 -0500

    Prueba conexion BD MSSQL endpoint get employees

--- src/main.go
+++ src/main.go
@@ -1,27 +1,55 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"context"
 	"database/sql"
 	"fmt"
-
-	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 
 	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/gin-gonic/gin"
 )
 
 var db *sql.DB
-var server = "backofficedb"
+var server = "backofficedb.database.windows.net"
 var port = 1433
 var user = "backoffice"
 var password = "D4t4c3nt3rCl0ud"
 var database = "backofficeDB"
 
-func main() {
+type Employee struct {
+	Id       int    `json:"id" binding:"required"`
+	Name     string `json:"name" binding:"required"`
+	Location string `json:"location" binding:"required"`
+}
 
+func main() {
+	//Abrir conexion de base de datos
+	openConnection()
+	defineEndpointsAndRunApp()
+}
+func openConnection() {
+	// Build connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
+
+	var err error
+
+	// Create connection pool
+	db, err = sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal("Error creating connection pool: ", err.Error())
+	}
+	ctx := context.Background()
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Printf("Connected!\n")
+}
+
+func defineEndpointsAndRunApp() {
 	r := gin.Default()
 
 	r.GET("/hello", func(c *gin.Context) {
@@ -52,48 +80,32 @@ func main() {
 		})
 	})
 
-	r.GET("/products", GetProducts)
-
+	r.GET("/employees", GetEmployees)
 	r.Run() // listen and serve on 0.0.0.0:8080
+
 }
 
-func GetProducts(c *gin.Context) {
+func GetEmployees(c *gin.Context) {
 	//Modificado para conectar con base de datos SQL Server
 
-	// Build connection string
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
-		server, user, password, port, database)
-
-	var err error
-
-	// Create connection pool
-	db, err = sql.Open("sqlserver", connString)
-	if err != nil {
-		log.Fatal("Error creating connection pool: ", err.Error())
-	}
-	ctx := context.Background()
-	err = db.PingContext(ctx)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	fmt.Printf("Connected!\n")
-
 	// Read employees
-	count, err := ReadEmployees()
+	var employees []Employee
+	count, err, employees := ReadEmployees()
 	if err != nil {
 		log.Fatal("Error reading Employees: ", err.Error())
 	}
+	c.JSON(http.StatusOK, employees)
 	fmt.Printf("Read %d row(s) successfully.\n", count)
 
 }
 
-func ReadEmployees() (int, error) {
+func ReadEmployees() (int, error, []Employee) {
 	ctx := context.Background()
-
+	var employees []Employee
 	// Check if database is alive.
 	err := db.PingContext(ctx)
 	if err != nil {
-		return -1, err
+		return -1, err, employees
 	}
 
 	tsql := fmt.Sprintf("SELECT Id, Name, Location FROM SalesLT.Employees;")
@@ -101,27 +113,22 @@ func ReadEmployees() (int, error) {
 	// Execute query
 	rows, err := db.QueryContext(ctx, tsql)
 	if err != nil {
-		return -1, err
+		return -1, err, employees
 	}
 
 	defer rows.Close()
 
 	var count int
-
 	// Iterate through the result set.
 	for rows.Next() {
-		var name, location string
-		var id int
-
+		var emp Employee
 		// Get values from row.
-		err := rows.Scan(&id, &name, &location)
+		err := rows.Scan(&emp.Id, &emp.Name, &emp.Location)
 		if err != nil {
-			return -1, err
+			return -1, err, employees
 		}
-
-		fmt.Printf("ID: %d, Name: %s, Location: %s\n", id, name, location)
+		employees = append(employees, emp)
 		count++
 	}
-
-	return count, nil
+	return count, nil, employees
 }
